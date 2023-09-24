95d41fd6b7ea7764fb3cec0c4e37ef2ef434b283
Adrian Ross <amr424@lehigh.edu>
2023-09-23 23:49:37 -0400

    react-three-js add bananas

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,11 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+
+      <p> My name is <strong>Adrian Michael Ross</strong> and I am a fourth-year undergraduate and researcher at Lehigh University working towards a dually accredited honors degree in computer science and business.</p>
+      <p>I am studying at Lehigh University, PA, within the P.C. Rossin College of Engineering and Applied Sciences and the College of Business and Economics. My focuses are in &#x26D3; blockchain, &#x1F4B0; fintech, and &#x1F4CA; data science, doing research in the former and minors in the latter.</p>
+      <p>I also enjoy &#x2615; making coffee, &#x1F3BF; skiing, &#x1F373; cooking, and &#x1F3B9; playing the piano.</p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 // Section 1: Import Dependencies
 import * as React from 'react'
+import { Suspense, useState } from 'react'
 import { Link, StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import '../../global.css'
+
+import Bananas from '../components/Bananas'
+// Comment the above and uncomment the following to import the WebGL BG lazily for faster loading times
+// const Bananas = lazy(() => import('./Bananas'))
+
+import { FadeIn, LeftMiddle } from '../components/styles'
 
 // Section 2: Import Styling
 import {
@@ -30,9 +37,13 @@ const Headshot = () => {
 
 // Section 4: Main Component
 const IndexPage = () => {
-
+  const [speed, set] = useState(1)
   return (
     <>
+      <Suspense fallback={null}>
+        <Bananas speed={speed} />
+        <FadeIn />
+      </Suspense>
       <div style={{padding: 1+"em", paddingTop: 2+"em"}}>
         <hr style={{ border: 2+"px solid" }}></hr>
         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
@@ -64,9 +75,7 @@ const IndexPage = () => {
         <br />
         <p style={{ fontStyle: "italic", textAlign: "center", lineHeight: 1.5 }}>I am a, &#127891; senior <a href="https://csb.lehigh.edu">B.S. Honors Computer & Business</a> @ <a href="https://www.lehigh.edu">Lehigh University</a>, &#x1F52C; Researcher @ <a href="https://sss.cse.lehigh.edu">Scalable Systems & Software Research Group</a> in <a href="https://blockchain.cse.lehigh.edu">Blockchain Lab</a>, and Fmr. President of the <a href="https://csb.lehigh.edu/csba">CSBA</a>!</p>
         <br /> 
-        <p>Thanks for popping by! My name is <strong>Adrian Michael Ross</strong> and I am a fourth-year undergraduate and researcher at Lehigh University working towards a dually accredited honors degree in computer science and business.</p>
-        <p>I am studying at Lehigh University, PA, within the P.C. Rossin College of Engineering and Applied Sciences and the College of Business and Economics. My focuses are in &#x26D3; blockchain, &#x1F4B0; fintech, and &#x1F4CA; data science, doing research in the former and minors in the latter.</p>
-        <p>I also enjoy &#x2615; making coffee, &#x1F3BF; skiing, &#x1F373; cooking, and &#x1F3B9; playing the piano.</p>
+        <p>Thanks for popping by!</p>
         <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
       </Layout>
     </>
